cf619b42d07d8e57231b8beffe77a37ddcba8a9d
Devin Bayly <baylyd@email.arizona.edu>
2020-02-13 10:47:20 -0700

    starting out

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name;
+	import Asyncer from "./Asyncer.svelte"
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+<Asyncer/>
---
+++ src/Asyncer.svelte
@@ -0,0 +1,22 @@
+<script>
+// call async function
+let promise = getNumber()
+function delay(ms) {
+    return new Promise(res=>setTimeout(res,ms))
+}
+async function getNumber() {
+    return await delay(1000).then(res=> Math.floor(Math.random()*10))
+}
+// put the returning function as the resolve in the promise returned
+
+let handleClick =()=> {
+    promise = getNumber()
+}
+</script>
+
+<button on:click={handleClick}>Change</button>
+{#await promise}
+<p>waiting ...</p>
+{:then number}
+<p>number is {number}</p>
+{/await}
